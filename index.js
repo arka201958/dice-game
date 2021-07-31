26dac746fd05bcfedb36f661647b9242822d96d9
Arka Chowdhury <55938145+arka201958@users.noreply.github.com>
2021-07-31 19:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var random1= Math.floor(Math.random()*6)+1;
-var rimage1="images/dice"+random1+".png";
+var rimage1="/dice"+random1+".png";
 var image1= document.querySelectorAll("img")[0];
 image1.setAttribute("src",rimage1);
 
 var random2= Math.floor(Math.random()*6)+1;
-var rimage2="images/dice"+random2+".png";
+var rimage2="/dice"+random2+".png";
 var image2= document.querySelectorAll("img")[1];
 image2.setAttribute("src",rimage2);
 
